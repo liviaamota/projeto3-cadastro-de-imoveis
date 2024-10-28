fca71bc5caad434ba31b2fe3df73636861123df5
liviaamota <livia.mota11@aluno.ce.gov.br>
2024-10-28 16:58:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let option = ''
+
+do {
+    option = prompt(`cadastro de imoveis${deck.length}
+    1 - quantidades de imoveis
+    2 - salvar um novo imovel
+    3 -  listar os imóveis 
+    4 - sair 
+    `)
+    
+    switch (option){
+   
+   
+    }
+ } while (option !== '3')
+
+const realEstate = { 
+    name: 'John',
+    number of rooms : 3 
+    number of bathrooms : 2 
+    garage : 'nao '
+}
